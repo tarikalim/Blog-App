d536664c0ae79125764c1590b732355d7b3a05e5
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-04-23 18:25:11 +0300

    namespaces

--- create_app.py
+++ create_app.py
@@ -2,8 +2,8 @@ from extensions import db, mail, jwt, migrate, api
 from config import Config
 from flask import Flask
 from API.AuthAPI import auth_ns
-
-
+from API.UserAPI import user_ns
+from API.PostAPI import post_ns
 
 
 def create_app():
@@ -11,6 +11,8 @@ def create_app():
     app.config.from_object(Config)
 
     api.add_namespace(auth_ns, path='/auth')
+    api.add_namespace(user_ns, path='/user')
+    api.add_namespace(post_ns, path='/post')
 
     mail.init_app(app)
     db.init_app(app)
