e868a26919321fce4cd2db66b829ac788d3e8dd6
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-04-23 18:24:26 +0300

    crud operation for user model

---
+++ Service/UserService.py
@@ -0,0 +1,31 @@
+from model.model import db, User
+
+
+class UserService:
+    @staticmethod
+    def get_user_by_id(user_id):
+        return User.query.get(user_id)
+
+    @staticmethod
+    def get_user_by_username(username):
+        return User.query.filter_by(username=username).first()
+
+    @staticmethod
+    def get_all_users():
+        return User.query.all()
+
+    @staticmethod
+    def update_user(user_id, username=None, email=None):
+        user = User.query.get(user_id)
+        if username:
+            user.username = username
+        if email:
+            user.email = email
+        db.session.commit()
+        return user
+
+    @staticmethod
+    def delete_user(user_id):
+        user = User.query.get(user_id)
+        db.session.delete(user)
+        db.session.commit()
