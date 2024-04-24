cd46af006ad7f2dcc6b9df738a45dd0909c48936
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-04-24 23:40:57 +0300

    debug like service

--- Service/LikeService.py
+++ Service/LikeService.py
@@ -5,6 +5,10 @@ class LikeService:
 
     @staticmethod
     def create_like(user_id, post_id):
+        existing_like = Like.query.filter_by(user_id=user_id, post_id=post_id).first()
+        if existing_like:
+            return None  # Post already liked
+
         new_like = Like(
             user_id=user_id,
             post_id=post_id
@@ -14,22 +18,14 @@ class LikeService:
         return new_like
 
     @staticmethod
-    def delete_like(like_id, user_id):
-        like = Like.query.get(like_id)
-        if like and like.user_id == user_id:
+    def delete_like(user_id, post_id):
+        like = Like.query.filter_by(user_id=user_id, post_id=post_id).first()
+        if like:
             db.session.delete(like)
             db.session.commit()
             return True, "Like deleted successfully"
-        return False, "You are not the owner of this like"
+        return False, "Failed to delete like"
 
     @staticmethod
     def get_post_likes(post_id):
         return Like.query.filter_by(post_id=post_id).count()
-
-    @staticmethod
-    def get_user_likes(user_id):
-        return Like.query.filter_by(user_id=user_id).all()
-
-    @staticmethod
-    def get_all_likes():
-        return Like.query.all()
