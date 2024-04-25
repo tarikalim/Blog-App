76e3e4ce649cb37f93752296e87800958325eb29
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-04-25 10:29:10 +0300

    search posts by title

--- API/PostAPI.py
+++ API/PostAPI.py
@@ -31,6 +31,20 @@ find_post_model = post_ns.model('FindPost', {
 # posts related operations
 @post_ns.route('')
 class PostsResource(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('title', type=str, help='Title to filter posts', location='args')
+
+    # get all posts or filter by title
+    @post_ns.expect(parser)
+    @post_ns.marshal_list_with(find_post_model)
+    def get(self):
+        args = self.parser.parse_args()
+        title = args['title']
+        if title:
+            posts = PostService.get_posts_by_title(title)
+        else:
+            posts = PostService.get_all_posts()
+        return posts
 
     # create a post
     @jwt_required()
@@ -87,20 +101,3 @@ class PostResource(Resource):
 
         PostService.delete_post(post_id)
         return {'message': 'Post deleted successfully.'}, 200
-
-
-@post_ns.route('/search')
-class PostsResource(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('title', type=str, help='Title to filter posts')
-
-    # get all posts or filter by title
-    @post_ns.expect(parser)
-    @post_ns.marshal_list_with(find_post_model)
-    def get(self):
-        title = self.parser.parse_args().get('title')
-        if title:
-            posts = PostService.get_posts_by_title(title)
-        else:
-            posts = PostService.get_all_posts()
-        return posts
