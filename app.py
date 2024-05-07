6df4a41bdb37b413f816be3ab954e2eead6c765a
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-05-07 19:36:41 +0300

    .

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from dotenv import load_dotenv
-
 load_dotenv()
 from create_app import create_app
 from extensions import db
 
+
 app = create_app()
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
