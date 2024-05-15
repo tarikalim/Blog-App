7080210474a36561126f16a3a68002d7aa45f846
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-05-15 15:48:22 +0300

    debug

--- Exception/error_handler.py
+++ Exception/error_handler.py
@@ -1,4 +1,6 @@
+from flask_jwt_extended.exceptions import JWTExtendedException
 from flask_restx import Api
+
 from Exception.exception import ApplicationException
 
 
@@ -6,3 +8,10 @@ def error_handler(api: Api):
     @api.errorhandler(ApplicationException)
     def handle_application_exception(error):
         return {'message': error.message}, error.status_code
+
+    @api.errorhandler(Exception)
+    def handle_exception(error):
+        if isinstance(error, (ApplicationException, JWTExtendedException)):
+            raise error
+        else:
+            return {'message': 'An unexpected error occurred', 'details': str(error)}, 500
