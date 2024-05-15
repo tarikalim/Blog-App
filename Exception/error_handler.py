ca00629da71ea6f72e10fb074271f259ddf9e065
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-05-16 01:27:48 +0300

    error_handler.py

--- Exception/error_handler.py
+++ Exception/error_handler.py
@@ -9,17 +9,9 @@ from extensions import db
 def error_handler(api: Api):
     @api.errorhandler(ApplicationException)
     def handle_application_exception(error):
-
         return {'message': error.message}, error.status_code
 
     @api.errorhandler(SQLAlchemyError)
     def handle_database_error(error):
         db.session.rollback()
         return {'message': 'A database error occurred', 'details': str(error)}, 500
-
-    @api.errorhandler(Exception)
-    def handle_exception(error):
-        if isinstance(error, (ApplicationException, JWTExtendedException, SQLAlchemyError)):
-            raise error
-        else:
-            return {'message': 'An unexpected error occurred', 'details': str(error)}, 500
