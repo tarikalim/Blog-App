2f0650879858eca1eb65b324b0ec94ab27ad4ba3
MevlutKorkmaz <korkmazme@mef.edu.tr>
2024-05-17 10:59:45 +0300

    post details

--- static/js/post_detail.js
+++ static/js/post_detail.js
@@ -1,4 +1,7 @@
 let isLiked = false;
+let isFavorite = false;
+let favoriteId = null;
+
 document.addEventListener('DOMContentLoaded', function () {
     const queryParams = new URLSearchParams(window.location.search);
     const postId = queryParams.get('post_id');
@@ -9,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
     fetchPostDetails(postId);
     setupEventListeners(postId);
     setupLikeButton(postId);
+    setupFavoriteButton(postId);
 });
 
 function setupEventListeners(postId) {
@@ -29,10 +33,11 @@ function setupEventListeners(postId) {
             const dropdownContent = this.nextElementSibling;
             if (dropdownContent.style.display === 'block') {
                 dropdownContent.style.display = 'none';
+            } else {
+                dropdownContent.style.display = 'block';
             }
         });
     });
-
 
     const commentList = document.getElementById('commentList');
     commentList.addEventListener('click', event => {
@@ -49,15 +54,14 @@ function setupEventListeners(postId) {
     });
 }
 
-
 function fetchPostDetails(postId) {
-    fetch(`/post/${postId}`, {method: 'GET'})
+    fetch(`/post/${postId}`, { method: 'GET' })
         .then(response => response.json())
         .then(data => {
             if (data.error) {
                 throw new Error(data.error);
             }
-            const {title, content, publish_date, category_name} = data;
+            const { title, content, publish_date, category_name } = data;
             document.getElementById('postTitle').textContent = title;
             document.getElementById('postContent').textContent = content;
             document.getElementById('publishDate').textContent = new Date(publish_date).toLocaleDateString();
@@ -91,9 +95,8 @@ function updateLikes(postId) {
         });
 }
 
-
 function updateComments(postId) {
-    fetch(`/comment/${postId}`, {method: 'GET'})
+    fetch(`/comment/${postId}`, { method: 'GET' })
         .then(response => response.json())
         .then(data => {
             if (data.error) {
@@ -108,7 +111,6 @@ function updateComments(postId) {
 
                 const commentContainer = document.createElement('div');
                 commentContainer.className = 'comment-container';
-
 
                 const commentInfo = document.createElement('p');
                 commentInfo.textContent = `Commented by: ${comment.username}`;
@@ -127,12 +129,12 @@ function updateComments(postId) {
                 const actions = document.createElement('div');
                 actions.className = 'dropdown';
                 actions.innerHTML = `
-        <div class="dropbtn">⋮</div>
-        <div class="dropdown-content">
-            <a href="#" data-action="update">Update</a>
-            <a href="#" data-action="delete">Delete</a>
-        </div>
-    `;
+                    <div class="dropbtn">⋮</div>
+                    <div class="dropdown-content">
+                        <a href="#" data-action="update">Update</a>
+                        <a href="#" data-action="delete">Delete</a>
+                    </div>
+                `;
 
                 li.appendChild(commentContainer);
                 li.appendChild(commentDate);
@@ -140,12 +142,9 @@ function updateComments(postId) {
 
                 commentList.appendChild(li);
             });
-
-
         })
         .catch(error => console.error('Error updating comments:', error));
 }
-
 
 function postComment(postId, content) {
     const token = localStorage.getItem('token');
@@ -155,7 +154,7 @@ function postComment(postId, content) {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${token}`
         },
-        body: JSON.stringify({content: content})
+        body: JSON.stringify({ content: content })
     })
         .then(response => {
             if (!response.ok) {
@@ -189,7 +188,7 @@ function deleteComment(commentId, postId) {
         })
         .catch(error => {
             console.error('Error deleting comment:', error);
-            alert('You can only delete your own comments.');
+            alert(error.message);
         });
 }
 
@@ -218,7 +217,6 @@ function updateLikeButton(postId) {
     }
 }
 
-
 function likePost(postId) {
     const token = localStorage.getItem('token');
     fetch(`/like/${postId}`, {
@@ -240,7 +238,6 @@ function likePost(postId) {
             alert('Failed to like post. Please try again.');
         });
 }
-
 
 function unlikePost(postId) {
     const token = localStorage.getItem('token');
@@ -264,7 +261,6 @@ function unlikePost(postId) {
         });
 }
 
-
 function fetchLikeStatus(postId) {
     const token = localStorage.getItem('token');
     return fetch(`/like/status/${postId}`, {
@@ -284,5 +280,103 @@ function fetchLikeStatus(postId) {
         })
         .catch(error => {
             console.error('Error fetching like status:', error);
+        });
+}
+
+function setupFavoriteButton(postId) {
+    fetchFavoriteStatus(postId).then(favoriteStatus => {
+        isFavorite = favoriteStatus.status;
+        favoriteId = favoriteStatus.favorite_id;
+        updateFavoriteButton();
+    });
+}
+
+function updateFavoriteButton() {
+    const favoriteButton = document.getElementById('favoriteButton');
+
+    favoriteButton.onclick = function () {
+        const queryParams = new URLSearchParams(window.location.search);
+        const postId = queryParams.get('post_id');
+
+        if (isFavorite) {
+            unfavoritePost(postId);
+        } else {
+            favoritePost(postId);
+        }
+    };
+
+    if (isFavorite) {
+        favoriteButton.textContent = 'Unfavorite Post';
+    } else {
+        favoriteButton.textContent = 'Favorite Post';
+    }
+}
+
+function favoritePost(postId) {
+    const token = localStorage.getItem('token');
+    fetch(`/favorite/${postId}`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to favorite post');
+            }
+            return response.json();
+        })
+        .then(data => {
+            isFavorite = true;
+            favoriteId = data.id;
+            updateFavoriteButton();
+        })
+        .catch(error => {
+            console.error('Error favoriting post:', error);
+            alert('Failed to favorite post. Please try again.');
+        });
+}
+
+function unfavoritePost(postId) {
+    const token = localStorage.getItem('token');
+    fetch(`/favorite/${favoriteId}`, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to unfavorite post');
+            }
+            isFavorite = false;
+            favoriteId = null;
+            updateFavoriteButton();
+        })
+        .catch(error => {
+            console.error('Error unfavoriting post:', error);
+            alert('Failed to unfavorite post. Please try again.');
+        });
+}
+
+function fetchFavoriteStatus(postId) {
+    const token = localStorage.getItem('token');
+    return fetch(`/favorite/status/${postId}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch favorite status');
+            }
+            return response.json();
+        })
+        .then(data => {
+            return { status: data.status, favorite_id: data.favorite_id };
+        })
+        .catch(error => {
+            console.error('Error fetching favorite status:', error);
         });
 }
